38116ec4fdf62e0450d171303c85d61a3be91b25
ArpanaMahajan30 <73541904+ArpanaMahajan30@users.noreply.github.com>
2026-07-21 17:47:33 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,16 @@
+# 1. Ask user for age.
+#age = int(input("Enter your age: "))
+#print("You will be",age+1,"next year")
+
+# 2. Ask user for number and print whether it is Positive orNegative or Zero
+number = int(input("Enter a number: "))
+if number > 0:
+    print("Positive")
+elif number < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+# 3. ask user for marks and print if its A or B or C or D or Fail
+#marks = input("Enter your grade: ")
+#print(marks)
