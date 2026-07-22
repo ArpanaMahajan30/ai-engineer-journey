ae6e2eb1aefe3744a239b7237146b0aa4f3a0ad3
Arpana Mahajan <arpana1701972@gmail.com>
2026-07-22 18:40:29 +0530

    improved code

---
+++ month1/day1/practice.py
@@ -0,0 +1,30 @@
+"""
+
+# 1. Ask user for age.
+age = int(input("Enter your age: "))
+print("You will be",age+1,"next year")
+
+# 2. Ask user for number and print whether it is Positive orNegative or Zero
+number = int(input("Enter a number: "))
+if number > 0:
+    print("Positive")
+elif number < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+# 3. ask user for marks and print if its A or B or C or D or Fail
+marks = input("Enter your grade: ")
+if marks == "A":
+    print("Excellent")  
+elif marks == "B":
+    print("Good")
+elif marks == "C":
+    print("Average")    
+elif marks == "D":
+    print("Below Average")
+else:
+    print("Fail")
+
+"""
+
